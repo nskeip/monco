a2dbe62169da78f418498785ae7156bb39fe2368
Nikita Hismatov <hismatovns@gmail.com>
2023-12-20 02:02:15 +0400

    basic framework

---
+++ Makefile
@@ -0,0 +1,34 @@
+# Default compiler
+COMPILER ?= clang
+
+# Compilation flags
+CFLAGS = -Wall -Wextra -g
+
+# Source files
+SRC_FILES = main.c
+
+# Object files
+OBJ_FILES = $(SRC_FILES:.c=.o)
+
+# Output executable
+EXECUTABLE = monco-exe
+
+# Targets
+all: $(EXECUTABLE)
+
+$(EXECUTABLE): $(OBJ_FILES)
+	$(COMPILER) $(CFLAGS) $^ -o $@
+
+%.o: %.c
+	$(COMPILER) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ_FILES) $(EXECUTABLE)
+
+test: clean $(EXECUTABLE)
+	./$(EXECUTABLE) -t
+
+run: $(EXECUTABLE)
+	./$(EXECUTABLE)
+
+.PHONY: all clean test run
---
+++ main.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void run_tests() {
+  assert(1 == 1);
+  printf("\x1b[32m"); // green text
+  printf("\u2713 ");  // Unicode check mark
+  printf("\x1b[0m");  // Reset text color to default
+  printf("All tests passed\n");
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    puts("Usage:");
+    printf("%15s    %s\n", "-t, --tests", "Run tests");
+    return EXIT_FAILURE;
+  }
+  if (strcmp(argv[1], "--test") == 0 || strcmp(argv[1], "-t") == 0) {
+    run_tests();
+    return 0;
+  }
+  return EXIT_SUCCESS;
+}
